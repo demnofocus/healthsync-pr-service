fed5460851d9478946c7961f4a54bf07c29aa677
Kevin Kularathne <47452568+demnofocus@users.noreply.github.com>
2024-11-27 07:00:11 +0530

    Initial project

---
+++ src/main/java/com/meditrack/meditrack_pr_service/MeditrackPrServiceApplication.java
@@ -0,0 +1,13 @@
+package com.meditrack.meditrack_pr_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeditrackPrServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeditrackPrServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meditrack/meditrack_pr_service/MeditrackPrServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meditrack.meditrack_pr_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeditrackPrServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
